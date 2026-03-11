bed015d0df4fc79805eb5d60ad27e16ec1276206
solremifa <haha95578@gmail.com>
2026-03-11 15:29:28 +0900

    20260311.pj3

--- src/ch02_pjt_01/MainClass.java
+++ src/ch02_pjt_01/MainClass.java
@@ -3,10 +3,17 @@ package ch02_pjt_01;
 public class MainClass {
     public static void main(String[] args) {
 
-        MyCalculator calculator = new MyCalculator();
-        calculator.CalAdd(10, 5, new CalAdd());
-        calculator.CalSub(10, 5, new CalSub());
-        calculator.CalMul(10, 5, new CalMul());
-        calculator.CalDiv(10, 5, new CalDiv());
+        new CalAssembler();
+//        MyCalculator calculator = new MyCalculator();
+//        calculator.calculate(10, 5, new CalAdd());
+//        calculator.calculate(10, 5, new CalSub());
+//        calculator.calculate(10, 5, new CalMul());
+//        calculator.calculate(10, 5, new CalDiv());
+
+
+//        calculator.CalAdd(10, 5, new CalAdd());
+//        calculator.CalSub(10, 5, new CalSub());
+//        calculator.CalMul(10, 5, new CalMul());
+//        calculator.CalDiv(10, 5, new CalDiv());
     }
 }
--- src/ch02_pjt_01/MyCalculator.java
+++ src/ch02_pjt_01/MyCalculator.java
@@ -2,23 +2,28 @@ package ch02_pjt_01;
 
 public class MyCalculator {
 
-    public void CalAdd(int fNum, int sNum, CalAdd calAdd) {
-        int value = calAdd.doOperation(fNum, sNum);
+    public void calculate(int fNum, int sNum, ICalculator calculator) {
+        int value = calculator.doOperation(fNum, sNum);
         System.out.println("result: " + value);
     }
 
-    public void CalSub(int fNum, int sNum, CalSub CalSub) {
-        int value = CalSub.doOperation(fNum, sNum);
-        System.out.println("result: " + value);
-    }
-
-    public void CalMul(int fNum, int sNum, CalMul CalMul) {
-        int value = CalMul.doOperation(fNum, sNum);
-        System.out.println("result: " + value);
-    }
-
-    public void CalDiv(int fNum, int sNum, CalDiv CalDiv) {
-        int value = CalDiv.doOperation(fNum, sNum);
-        System.out.println("result: " + value);
-    }
+//    public void CalAdd(int fNum, int sNum, CalAdd calAdd) {
+//        int value = calAdd.doOperation(fNum, sNum);
+//        System.out.println("result: " + value);
+//    }
+//
+//    public void CalSub(int fNum, int sNum, CalSub CalSub) {
+//        int value = CalSub.doOperation(fNum, sNum);
+//        System.out.println("result: " + value);
+//    }
+//
+//    public void CalMul(int fNum, int sNum, CalMul CalMul) {
+//        int value = CalMul.doOperation(fNum, sNum);
+//        System.out.println("result: " + value);
+//    }
+//
+//    public void CalDiv(int fNum, int sNum, CalDiv CalDiv) {
+//        int value = CalDiv.doOperation(fNum, sNum);
+//        System.out.println("result: " + value);
+//    }
 }
