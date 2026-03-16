5a79dd97ea89931c78525da3b5e2926712265e42
solremifa <haha95578@gmail.com>
2026-03-16 13:34:19 +0900

    maven_study202603

---
+++ maven-study/src/main/java/ch03_pjt_01/MainClass.java
@@ -0,0 +1,8 @@
+package ch03_pjt_01;
+
+public class MainClass {
+    public static void main(String[] args) {
+        TransportationWalk transportationWalk = new TransportationWalk();
+        transportationWalk.move();
+    }
+}
