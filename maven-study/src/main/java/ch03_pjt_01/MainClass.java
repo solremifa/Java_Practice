cdc53e270cb61e563d118a4fcf159fa97e3c48ae
solremifa <haha95578@gmail.com>
2026-03-18 13:18:13 +0900

    maven_study202603

--- maven-study/src/main/java/ch03_pjt_01/MainClass.java
+++ maven-study/src/main/java/ch03_pjt_01/MainClass.java
@@ -1,8 +1,17 @@
 package ch03_pjt_01;
 
+import org.springframework.context.support.GenericXmlApplicationContext;
+
 public class MainClass {
     public static void main(String[] args) {
-        TransportationWalk transportationWalk = new TransportationWalk();
+
+        // 개발자인 우리가 직접 객체화
+//        TransportationWalk transportationWalk = new TransportationWalk();
+//        transportationWalk.move();
+
+        // applicationContext.xml을 사용해서 객체화
+        GenericXmlApplicationContext ctx = new GenericXmlApplicationContext("classpath:applicationContext.xml");
+        TransportationWalk transportationWalk = ctx.getBean("tWalk", TransportationWalk.class);
         transportationWalk.move();
     }
 }
